17cc283d313c2c3a88f675de210a997ba9f8fa82
arissatya <arissatya.nugraha@gmail.com>
2020-08-10 17:11:20 +0700

    fixing port

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(cors())
 app.use('/', index);
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3000 
+
+app.listen(PORT, () => {
   console.log(`online!`);
 })
